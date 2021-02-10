9744cad36faad81db0d67cc94281e77d38752be8
sconnell <sconnell@ceiamerica.com>
2021-02-10 11:18:55 -0500

    Update loginUser

--- src/app/user/auth/auth.service.ts
+++ src/app/user/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { IUser } from '../user.model';
 
 @Injectable({
@@ -16,10 +16,9 @@ export class AuthService {
     let loginInfo = { username: username, password: password };
     let options = { headers: new HttpHeaders({'Content-Type': 'application/json'}) };
 
-    // fix this (tap is fine)
     return this.http.post('/api/login', loginInfo, options)
-      .pipe(tap(data => {
-        //this.currentUser = <IUser>data['user'];
+      .pipe(map((data: any) => {
+        ({...data, user: this.currentUser = data.user})
       }))
       .pipe(catchError(err => {
         return of(false);
